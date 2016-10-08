c26c94a9ba5fe7b58e956fcc585bf13d23ddd5ae
Abrar <er.abrar@gmail.com>
2016-10-08 09:44:41 -0400

    Update Product.php

--- Week 11/ajaxproductsupdate/Product.php
+++ Week 11/ajaxproductsupdate/Product.php
@@ -2,11 +2,7 @@
 
 class Product
 {
-    /**
-     FIrst she created without class and then she created class and copy one by one query make
-    function and call that function from where you deleted this code  and pest it in to class..call
-    at function at that place
-    **/
+    
     public function addProduct($pname,$pcat){
         $db = Database::getDB();
         $sql = "INSERT INTO mproducts (productName,category) VALUES ('$pname', '$pcat')";
